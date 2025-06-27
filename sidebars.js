ac3d7d0d93658bebf74be478eeee3e9e716f457b
julien-odo <julien.odo@codigo.ai>
2025-06-27 15:09:48 +0200

    escrow guide tutorial (#50)

--- sidebars.js
+++ sidebars.js
@@ -76,6 +76,7 @@ const sidebars = {
             "guides/solana/build-and-deploy",
             "guides/solana/ai-autocomplete",
             "guides/solana/unit-tests",
+            "guides/solana/escrow-program",
             //"guides/integrating-the-sdk",
             // "guides/solana-linked-seeds",
             // "guides/state-compression",
